45ddfe305f4d157a9344f2a770e79cc8b9afc970
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2020-12-22 12:26:49 -0500

    Global redirector insead of nd

--- mre_run.py
+++ mre_run.py
@@ -4,8 +4,7 @@ import mre_processor
 
 flist = {
     'TTTo2L2Nu' : [
-        'root://ndcms.crc.nd.edu//store/mc/RunIISummer19UL17NanoAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mc2017_realistic_v6-v1/260000/10470AF9-B021-3742-A885-12D32B903131.root'
-        #'root://global.cern.ch//store/mc/RunIISummer19UL17NanoAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mc2017_realistic_v6-v1/260000/10470AF9-B021-3742-A885-12D32B903131.root'
+        'root://cms-xrd-global.cern.ch//store/mc/RunIISummer19UL17NanoAOD/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mc2017_realistic_v6-v1/260000/10470AF9-B021-3742-A885-12D32B903131.root'
     ]
 }
 
